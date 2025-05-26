e76363d1e7272e48f282da86a12376984a9f75d3
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-05-26 23:47:08 +0300

    feat: update passthough example

--- examples/passthrough/main.go
+++ examples/passthrough/main.go
@@ -9,35 +9,48 @@ import (
 
 const (
 	FONT_WIDTH  = 18
-	FONT_HEIGHT = 46
+	FONT_HEIGHT = 36
 	N_CHANNELS  = 4
 )
 
 func main() {
-	pixels, width, height, err := chafa.Load("./examples/image.png")
+	if len(os.Args) < 2 {
+		return
+	}
+
+	pixels, width, height, err := chafa.Load(os.Args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 
+	termInfo := chafa.TermDbDetect(chafa.TermDbGetDefault(), os.Environ())
+	defer chafa.TermInfoUnref(termInfo)
+
+	mode := chafa.TermInfoGetBestCanvasMode(termInfo)
+	pixelMode := chafa.TermInfoGetBestPixelMode(termInfo)
+
+	passthrough := chafa.CHAFA_PASSTHROUGH_NONE
+	if chafa.TermInfoGetIsPixelPassthroughNeeded(termInfo, pixelMode) {
+		passthrough = chafa.TermInfoGetPassthroughType(termInfo)
+	}
+
 	config := chafa.CanvasConfigNew()
 	defer chafa.CanvasConfigUnref(config)
 
-	chafa.CanvasConfigSetGeometry(config, 40, 15)
+	chafa.CanvasConfigSetGeometry(config, 40, 20)
 	chafa.CanvasConfigSetCellGeometry(config, FONT_WIDTH, FONT_HEIGHT)
-	chafa.CanvasConfigSetPixelMode(config, chafa.CHAFA_PIXEL_MODE_KITTY)
-	chafa.CanvasConfigSetPassthrough(config, chafa.CHAFA_PASSTHROUGH_TMUX)
-
-	widthNew := config.Width
-	heightNew := config.Height
+	chafa.CanvasConfigSetCanvasMode(config, mode)
+	chafa.CanvasConfigSetPixelMode(config, pixelMode)
+	chafa.CanvasConfigSetPassthrough(config, passthrough)
 
 	chafa.CalcCanvasGeometry(
 		width, height,
-		&widthNew, &heightNew,
+		&config.Width, &config.Height,
 		float32(FONT_WIDTH)/float32(FONT_HEIGHT),
 		false, false,
 	)
-	chafa.CanvasConfigSetGeometry(config, widthNew, heightNew)
+	chafa.CanvasConfigSetGeometry(config, config.Width, config.Height)
 
 	canvas := chafa.CanvasNew(config)
 	defer chafa.CanvasUnRef(canvas)
@@ -49,12 +62,19 @@ func main() {
 		height,
 		width*N_CHANNELS,
 	)
+	defer chafa.FrameUnref(frame)
 
 	img := chafa.ImageNew()
+	defer chafa.ImageUnref(img)
+
 	chafa.ImageSetFrame(img, frame)
 
 	placement := chafa.PlacementNew(img, 1)
+	defer chafa.PlacementUnref(placement)
+
 	chafa.CanvasSetPlacement(canvas, placement)
 
-	fmt.Println(chafa.CanvasPrint(canvas, nil))
+	gs := chafa.CanvasPrint(canvas, termInfo)
+
+	fmt.Println(gs)
 }
